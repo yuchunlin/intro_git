8996d7228ba3e4c6264025c65dbd46241e13ca16
Yu-Chun Lin <yu.chun.lin@berkeley.edu>
2015-01-27 21:43:08 -0800

    Fixed syntax error

--- introductions.py
+++ introductions.py
@@ -3,7 +3,8 @@ def introduce():
 	print "Hello, I'm Yu-Chun"
 	print "This should be the testing branch."
 	print("Hello, I'm Yu-Chun")
-	while i >= 10:
+	i = 0
+	while i < 10:
 		introduce()
 		i+=1
 	return "whhhhhyyyyy"
